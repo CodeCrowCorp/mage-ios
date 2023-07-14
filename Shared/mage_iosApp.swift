060d009715017513a23a9d91c8b27684c19eb804
Gagan Suie <gagan.suie@codecrow.io>
2023-07-14 02:51:06 -0500

    Initial Commit

---
+++ Shared/mage_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mage_iosApp.swift
+//  Shared
+//
+//  Created by Gagan Suie on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct mage_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
