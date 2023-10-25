6820c47d3d0a0e0ce4693022fb23feabd2f84cf7
Janak Jivrani <janak.jivrani@gmail.com>
2023-10-25 10:54:43 +0530

    - Delete Pods

--- Pods/Target Support Files/Pods-mage-ios (iOS)/Pods-mage-ios (iOS)-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mage_ios__iOS_ : NSObject
-@end
-@implementation PodsDummy_Pods_mage_ios__iOS_
-@end
--- Pods/Target Support Files/Pods-mage-ios (macOS)/Pods-mage-ios (macOS)-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mage_ios__macOS_ : NSObject
-@end
-@implementation PodsDummy_Pods_mage_ios__macOS_
-@end
